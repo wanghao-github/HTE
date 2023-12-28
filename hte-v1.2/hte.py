bc4912146de3408e590105a7ceef47e84b073184
Hao Wang <wanghao90515@163.com>
2023-12-28 10:46:21 +0100

    75 76

--- hte-v1.2/hte.py
+++ hte-v1.2/hte.py
@@ -2730,8 +2730,9 @@ class HTE(object):
                 os.chdir(sp)
                 dir_test = os.getcwd()
                 print "check_point74, here we change the dir now in:",dir_test
+                print "check_point75, before check the convergence, first we print the entry: ", self.structureDB[uid]
                 if self.structureDB[uid].check_convergency(calc_scheme_sp,calc):
-                    print "check_point75, here the structureDB[uid].check_convergency is Ture:"
+                    print "check_point76, here the structureDB[uid].check_convergency is Ture:"
                     calcpath=os.path.join(sp,uid,calc_scheme_sp)
                 os.chdir(parentdir)
                 if calcpath!=None:
