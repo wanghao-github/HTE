963a663a45fe4b078b3dbaa8c88a531345d89f19
Hao Wang <wanghao90515@163.com>
2024-01-09 01:20:06 +0100

    153

--- hte-v1.2/hte.py
+++ hte-v1.2/hte.py
@@ -4405,6 +4405,7 @@ class HTE(object):
     def get_magnetic_moments(self, uid, calc_scheme, magsettings={'submitted':True}, sub_directories={}):
         """return local magnetic moments (as list: [(atom,mom),...])"""
         prop_dict=self.get_properties(uid, calc_scheme, magsettings=magsettings, sub_directories=sub_directories)
+        print "check_point153,get_magnetic_moments,prop_dict is :",prop_dict
         moms=[]
         if ('magnetic_moments' in prop_dict) and ('chemical_symbols' in prop_dict):
             for i in range(len(prop_dict['chemical_symbols'])):
