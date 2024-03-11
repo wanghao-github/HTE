336eddfe467657a6eb1fdcd7fa7abed85a41ba41
Hao Wang <wanghao90515@163.com>
2024-03-11 19:25:17 +0100

    1

--- hte-v1.2/hte.py
+++ hte-v1.2/hte.py
@@ -1115,7 +1115,7 @@ class HTE(object):
                         line=line+pd['magnetic_moments']+' '
                     # elif subdir!={} and pd['']:
                         
-                    elif subdir != {} and ('updating' not in pd or pd['updating'] != 'true'):
+                    elif subdir != {} and pd['updating'] != True:
                         unique_key = next(iter(pd))
                         nested_pd = pd[unique_key] 
                         print "check_point162, subdir!={}! and is", subdir
