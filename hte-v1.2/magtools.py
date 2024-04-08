4999bc433492886ac33c3efa4a4c829ba9113074
Hao Wang <wanghao90515@163.com>
2024-04-08 22:31:03 +0200

    2

--- hte-v1.2/magtools.py
+++ hte-v1.2/magtools.py
@@ -479,7 +479,7 @@ class MSG(object):
         # pd = pd_temp_no_center
             
             
-        for j in range(pd_temp_no_center['scaled_positions']):
+        for j in range(len(pd_temp_no_center['scaled_positions'])):
             for g in msg.get_elements()[-(len(msg.get_elements()) - len(ciftags[id_field])):]:
                 print "check_point220 ,-(len(msg.get_elements()) - len(ciftags[id_field])): is ",g
                 npos2 = msg.symop_pos(g, (float(pd_temp_no_center['scaled_positions'][j][0]), float(pd_temp_no_center['scaled_positions'][j][1]), 
@@ -489,17 +489,17 @@ class MSG(object):
                 print "npos2 is ",npos2
                 print "nmom2 is ",nmom2
                 
-        #         is_new = True
+                is_new = True
                 
-        #         for px in pd['scaled_positions']:
-        #             if msg.is_equal_site(npos2, px):
-        #                 print "msg.is_equal_site "
-        #                 is_new = False
-        #                 break            
-        #         if is_new:
-        #             pd['chemical_symbols'].append(pd_temp_no_center['chemical_symbols'][j])
-        #             pd['scaled_positions'].append(npos2)
-        #             pd['initial_magnetic_moments'].append(nmom2)
+                for px in pd['scaled_positions']:
+                    if msg.is_equal_site(npos2, px):
+                        print "msg.is_equal_site "
+                        is_new = False
+                        break            
+                if is_new:
+                    pd['chemical_symbols'].append(pd_temp_no_center['chemical_symbols'][j])
+                    pd['scaled_positions'].append(npos2)
+                    pd['initial_magnetic_moments'].append(nmom2)
 
         #             pd_temp_no_center['chemical_symbols'].append(el)
         #             pd_temp_no_center['scaled_positions'].append(npos)
